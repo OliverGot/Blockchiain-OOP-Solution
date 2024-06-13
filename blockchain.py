84945b08af35676aff51b12e1752e3e7d4a0405b
Oliver Gotzinger <oliver.got@icloud.com>
2024-06-13 13:47:40 +1000

    added genesis block

--- blockchain.py
+++ blockchain.py
@@ -1,7 +1,12 @@
+import datetime
+
 class Block():
     def __init__(self, index, time, data, previousHash):
         self.index = index
         self.time = time
+        self.data = data
+        self.previousHash = previousHash
         self.next = None
 
+genesisBlock = Block(0, datetime.datetime.now(), "The first block!", "0")
 
