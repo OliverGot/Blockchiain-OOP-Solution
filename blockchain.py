1f777a83c412c4b33f8fcb4f05c3e7a27783f44e
Oliver Gotzinger <oliver.got@icloud.com>
2024-06-13 18:37:37 +1000

    added proof of work

--- blockchain.py
+++ blockchain.py
@@ -1,27 +1,38 @@
 import datetime
 import hashlib
 
+DIFFICULTY = 5
+
 class Block():
     def __init__(self, index, time, data, previousHash):
         self.index = index
         self.time = time
         self.data = data
+        self.nonce = 0
         self.previousHash = previousHash
         self.hash = self.calculateHash()
     
     def calculateHash(self):
-        blockString = str(self.index) + str(self.time) + str(self.data) + str(self.previousHash)
-        return hashlib.sha256(blockString.encode()).hexdigest()
-    
+        blockHash = hashlib.sha256()
+        blockHash.update(str(self.index).encode("utf-8") + str(self.time).encode("utf-8") + str(self.data).encode("utf-8") + str(self.previousHash).encode("utf-8") + str(self.nonce).encode("utf-8"))
+        return blockHash.hexdigest()
+
+    def mine(self, difficulty):
+        while self.hash[0:difficulty] != "0" * difficulty:
+            self.nonce += 1
+            self.hash = self.calculateHash()
+
     def __str__(self):
-        return f"Index:{self.index}\nTime:{self.time}\nData:{self.data}\nPrevious Hash:{self.previousHash}\nHash:{self.hash}"
+        return f"Index:{self.index}\nTime:{self.time}\nData:{self.data}\nNonce:{self.nonce}\nPrevious Hash:{self.previousHash}\nHash:{self.hash}"
 
 class Blockchain():
     def __init__(self):
         self.__chain = [Block(0, datetime.datetime.now(), "The first block!", "0")]
 
     def addBlock(self, data):
-        self.__chain.append(Block(len(self.__chain), datetime.datetime.now(), data, self.__chain[-1].calculateHash()))
+        newBlock = Block(len(self.__chain), datetime.datetime.now(), data, self.__chain[-1].hash)
+        newBlock.mine(DIFFICULTY)
+        self.__chain.append(newBlock)
 
     def getBlock(self, index):
         return self.__chain[index]
@@ -31,6 +42,17 @@ class Blockchain():
             if block.index != 0 and block.previousHash != self.__chain[block.index - 1].calculateHash():
                 return False
         return True
+
+class User():
+    def __init__(self, username, password, currency):
+        self.__username = username
+        self.__password = password
+        self.hash = self.calculateHash()
+    
+    def calculateHash(self):
+        blockHash = hashlib.sha256()
+        blockHash.update(str(self.__username).encode("utf-8") + str(self.__password).encode("utf-8"))
+        return blockHash.hexdigest()
 
 blockchain = Blockchain()
 blockchain.addBlock("hello")
