4be90cc0039895624ac6926b73555f0e438963a3
Oliver Gotzinger <oliver.got@icloud.com>
2024-06-13 17:34:24 +1000

    added block hash

--- blockchain.py
+++ blockchain.py
@@ -7,13 +7,14 @@ class Block():
         self.time = time
         self.data = data
         self.previousHash = previousHash
+        self.hash = self.calculateHash()
     
     def calculateHash(self):
         blockString = str(self.index) + str(self.time) + str(self.data) + str(self.previousHash)
         return hashlib.sha256(blockString.encode()).hexdigest()
     
     def __str__(self):
-        return f"Index:{self.index}\nTime:{self.time}\nData:{self.data}\nPrevious Hash:{self.previousHash}"
+        return f"Index:{self.index}\nTime:{self.time}\nData:{self.data}\nPrevious Hash:{self.previousHash}\nHash:{self.hash}"
 
 class Blockchain():
     def __init__(self):
@@ -35,6 +36,8 @@ blockchain = Blockchain()
 blockchain.addBlock("hello")
 blockchain.addBlock("data")
 blockchain.addBlock("Cool")
-print(blockchain.getBlock(1))
+for i in range(0, blockchain.getBlock(-1).index + 1):
+    print(blockchain.getBlock(i))
+    print()
 print(blockchain.isValid())
 
